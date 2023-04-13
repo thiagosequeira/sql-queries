ea2543c5dd184ab7e9be4ea3b50d64eb7d7fd0dc
Thiago Sequeira <thiagosequeira02@gmail.com>
2023-04-13 15:57:25 -0300

    Update queries.sql

--- Lección 2. Joins/queries.sql
+++ Lección 2. Joins/queries.sql
@@ -12,3 +12,29 @@ JOIN Track t ON pt.TrackId = t.TrackId
 JOIN Album a ON t.AlbumId = a.AlbumId 
 JOIN Artist a2 ON a.ArtistId = a2.ArtistId 
 JOIN Genre g ON t.GenreId = g.GenreId 
+
+-- Utilizar LEFT JOIN para obtener todos los datos de la tabla izquierda y los datos coincidentes de la tabla derecha
+
+SELECT c.CustomerId, c.FirstName, c.LastName, i.InvoiceId, i.InvoiceDate, i.Total
+FROM Customer c
+LEFT JOIN Invoice i ON c.CustomerId = i.CustomerId
+
+-- Utilizar RIGHT JOIN para obtener todos los datos de la tabla derecha y los datos coincidentes de la tabla izquierda
+
+SELECT i.InvoiceId, i.InvoiceDate, i.Total, c.CustomerId, c.FirstName, c.LastName
+FROM Invoice i
+RIGHT JOIN Customer c ON i.CustomerId = c.CustomerId
+
+-- Utilizar FULL OUTER JOIN para obtener todos los datos de ambas tablas
+
+SELECT c.CustomerId, c.FirstName, c.LastName, i.InvoiceId, i.InvoiceDate, i.Total
+FROM Customer c
+FULL OUTER JOIN Invoice i ON c.CustomerId = i.CustomerId
+
+/* Utilizar INNER JOIN para obtener sólo los datos coincidentes de ambas tablas */
+
+SELECT t.TrackId AS id_cancion, t.Name AS cancion, m.Name AS tipo_media, g.Name AS genero, il.UnitPrice AS precio
+FROM Track t
+INNER JOIN MediaType m ON t.MediaTypeId = m.MediaTypeId
+INNER JOIN Genre g ON t.GenreId = g.GenreId
+INNER JOIN InvoiceLine il ON t.TrackId = il.TrackId
