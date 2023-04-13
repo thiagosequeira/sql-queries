48b1009a37757c430c75ffa83061d8c6738b0664
Thiago Sequeira <thiagosequeira02@gmail.com>
2023-04-13 15:51:18 -0300

    Update queries.sql

--- Lección 1. Lectura de datos/queries.sql
+++ Lección 1. Lectura de datos/queries.sql
@@ -1,6 +1,6 @@
 /*
 Queries fundamentales SQL:
-• Lectura de datos de distintas tablas utilizando distintas condiciones.
+Lección 1. Lectura de datos de distintas tablas utilizando distintas condiciones.
 */
 
 SELECT *
@@ -11,6 +11,8 @@ FROM Customer c
 
 SELECT DISTINCT Company
 FROM Customer c 
+
+-- Utilización sentencia WHERE
 
 SELECT DISTINCT Company
 FROM Customer c 
@@ -28,6 +30,31 @@ SELECT DISTINCT Company, Country, State
 FROM Customer c 
 WHERE Company IS NOT NULL
 
+-- Seleccionar TOP 5 resultados
+
 SELECT *
 FROM Customer c 
 LIMIT 5
+
+-- Seleccionar columnas específicas de una tabla y renombrarlas
+
+SELECT FirstName AS Nombre, LastName AS Apellido, Email AS CorreoElectronico
+FROM Customer
+
+-- Ordenar resultados en orden ascendente o descendente 
+
+SELECT *
+FROM Customer
+ORDER BY LastName ASC
+
+SELECT *
+FROM Customer
+ORDER BY FirstName DESC
+
+-- Utilizar la función COUNT para contar el número de filas en una tabla
+
+SELECT COUNT(*)
+FROM Customer
+
+SELECT COUNT(DISTINCT Country)
+FROM Customer
