4d6f414e899b833a307debe39d33a8614153409d
Thiago Sequeira <thiagosequeira02@gmail.com>
2023-04-14 12:12:38 -0300

    Update queries.sql

--- Lección 2. Joins/queries.sql
+++ Lección 2. Joins/queries.sql
@@ -3,7 +3,7 @@ Queries fundamentales SQL:
 Lección 2. Joins.
 */
 
--- Join tablas playlist, track, album, artist y genre para saber todos los datos de cada canción que se encuentran en las distintas playlist disponibles
+-- INNER JOOIN tablas playlist, track, album, artist y genre para saber todos los datos de cada canción que se encuentran en las distintas playlist disponibles
 
 SELECT p.Name AS playlist, t.Name AS cancion, a.Title AS album, g.Name AS genero, a2.Name AS banda, t.Composer AS compositor, t.Milliseconds AS milisegundos
 FROM Playlist p 
@@ -13,25 +13,25 @@ JOIN Album a ON t.AlbumId = a.AlbumId
 JOIN Artist a2 ON a.ArtistId = a2.ArtistId 
 JOIN Genre g ON t.GenreId = g.GenreId 
 
--- Utilizar LEFT JOIN para obtener todos los datos de la tabla izquierda y los datos coincidentes de la tabla derecha
+-- LEFT JOIN para obtener todos los datos de la tabla izquierda y los datos coincidentes de la tabla derecha
 
 SELECT c.CustomerId, c.FirstName, c.LastName, i.InvoiceId, i.InvoiceDate, i.Total
 FROM Customer c
 LEFT JOIN Invoice i ON c.CustomerId = i.CustomerId
 
--- Utilizar RIGHT JOIN para obtener todos los datos de la tabla derecha y los datos coincidentes de la tabla izquierda
+-- RIGHT JOIN para obtener todos los datos de la tabla derecha y los datos coincidentes de la tabla izquierda
 
 SELECT i.InvoiceId, i.InvoiceDate, i.Total, c.CustomerId, c.FirstName, c.LastName
 FROM Invoice i
 RIGHT JOIN Customer c ON i.CustomerId = c.CustomerId
 
--- Utilizar FULL OUTER JOIN para obtener todos los datos de ambas tablas
+-- FULL OUTER JOIN para obtener todos los datos de ambas tablas
 
 SELECT c.CustomerId, c.FirstName, c.LastName, i.InvoiceId, i.InvoiceDate, i.Total
 FROM Customer c
 FULL OUTER JOIN Invoice i ON c.CustomerId = i.CustomerId
 
-/* Utilizar INNER JOIN para obtener sólo los datos coincidentes de ambas tablas */
+-- INNER JOIN para obtener sólo los datos coincidentes de ambas tablas 
 
 SELECT t.TrackId AS id_cancion, t.Name AS cancion, m.Name AS tipo_media, g.Name AS genero, il.UnitPrice AS precio
 FROM Track t
